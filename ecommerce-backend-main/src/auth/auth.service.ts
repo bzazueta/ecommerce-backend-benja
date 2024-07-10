cb48271eaf06242d45695b4194ed55e17d6a4b61
benjamin zazueta <benjamin.zazueta@serviciosexrtenos.com.mx>
2024-07-09 19:53:32 -0600

    first commit

---
+++ ecommerce-backend-main/src/auth/auth.service.ts
@@ -0,0 +1,104 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/user.entity';
+import { Repository, In } from 'typeorm';
+import { RegisterAuthDto } from './dto/register-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Rol } from '../roles/rol.entity';
+import { existsSync } from 'fs';
+
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        @InjectRepository(User) private usersRepository: Repository<User>,
+        @InjectRepository(Rol) private rolesRepository: Repository<Rol>,
+
+        private jwtService: JwtService
+    ) {}
+
+    async register(user: RegisterAuthDto) {
+        console.log("user",JSON.stringify(user))
+        const { email, phone } = user;
+        const emailExist = await this.usersRepository.findOneBy({ email: email })
+
+        if (emailExist) {
+            // 409 CONFLICT
+            throw new HttpException('El email ya esta registrado', HttpStatus.CONFLICT);
+        }
+
+        const phoneExist = await this.usersRepository.findOneBy({phone: phone});
+
+        if (phoneExist) {
+            throw new HttpException('El telefono ya esta registrado', HttpStatus.CONFLICT)
+        }
+
+        const newUser = this.usersRepository.create(user);
+        let rolesIds = [];
+        
+        if (user.rolesIds !== undefined && user.rolesIds !== null) { // DATA
+            rolesIds = user.rolesIds;
+        }
+        else {
+            rolesIds.push('CLIENT')
+        }
+        
+        const roles = await this.rolesRepository.findBy({ id: In(rolesIds) });
+        newUser.roles = roles;
+
+        const userSaved = await this.usersRepository.save(newUser);
+
+        const rolesString = userSaved.roles.map(rol => rol.id); //['CLIENT', 'ADMIN']
+        const payload = { id: userSaved.id, name: userSaved.name, roles: rolesString };
+        const token = this.jwtService.sign(payload);
+        const data = {
+            user: userSaved,
+            token: 'Bearer ' + token
+        }
+        delete data.user.password;
+        return data;
+    }
+
+    async login(loginData: LoginAuthDto) {
+        //console.log(`login data ${loginData.email} ${loginData.password}`);
+        const { email, password } = loginData;
+        const userFound = await this.usersRepository.findOne({ 
+            where: { email: email },
+            relations: ['roles']
+         })
+         console.log(`userFound ${JSON.stringify(userFound)}`);
+        if (!userFound) {
+            throw new HttpException('El email no existe', HttpStatus.NOT_FOUND);
+        }
+        // console.log(`password ${password}`);
+        // console.log(`userFound.password ${userFound.password}`); 
+        const isPasswordValid = await compare(password, userFound.password);
+        if (!isPasswordValid) {
+            console.log('PASSWORD INCORRECTO');
+            
+            // 403 FORBITTEN access denied
+            throw new HttpException('La contraseña es incorrecta', HttpStatus.FORBIDDEN);
+        }
+
+        const rolesIds = userFound.roles.map(rol => rol.id); //['CLIENT', 'ADMIN']
+
+        const payload = { 
+            id: userFound.id, 
+            name: userFound.name, 
+            roles: rolesIds 
+        };
+        const token = this.jwtService.sign(payload);
+        const data = {
+            user: userFound,
+            token: 'Bearer ' + token
+        }
+
+        delete data.user.password;
+        console.log('response login data ', data);
+        return data;
+    }
+
+}
---
+++ ecommerce-backend-main/src/config/config.ts
@@ -0,0 +1,7 @@
+export const MERCADO_PAGO_API = 'https://api.mercadopago.com/v1';
+export const MERCADO_PAGO_HEADERS = {
+    'Authorization': 'Bearer TEST-1779063627626035-052713-7858c9d7741600b2719a79ea02802dc9-635733978',
+    'Content-Type': 'application/json',
+    'X-Idempotency-Key':'b29d8c29-99e5-418a-9504-08f1aceed6d4'
+}
+export const API = "https://1715-187-251-251-237.ngrok-free.app/";
